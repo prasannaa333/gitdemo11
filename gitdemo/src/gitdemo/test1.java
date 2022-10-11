6d5f904b91cfd78a698b38ed3ce80165af4e18ca
prasanna <tdpkumar333@gmail.com>
2022-10-11 16:37:48 +0530

    first commit

---
+++ gitdemo/src/gitdemo/test1.java
@@ -0,0 +1,23 @@
+package gitdemo;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		
+		if(a>b) {
+			System.out.println("A is biggest nmber");
+			
+		}else
+		{
+			System.out.println("B is biggest number");
+			
+		}
+
+	
+
+	}
+
+}
