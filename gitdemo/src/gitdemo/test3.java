1f1c846f54283d21abaf58f0f720303761cb9db9
ramba <ramba@DESKTOP-7GVL3NF>
2022-10-15 11:28:40 +0530

    last commit

---
+++ gitdemo/src/gitdemo/test3.java
@@ -0,0 +1,20 @@
+package gitdemo;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c=30;
+		System.out.println(c);
+		if(a>b) {
+			System.out.println("A is biggest nmber");
+			
+		}else
+		{
+			System.out.println("B is biggest number");
+	}
+
+}
+}
