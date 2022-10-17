fda2a2de2816fd68c97da16fb3fc08ddc33bf6e6
Prathyusha <DELL@DESKTOP-8ALRF12>
2022-10-17 15:45:09 +0530

    This is the nwe one ....

---
+++ gitdemo/src/gitdemo/Test4.java
@@ -0,0 +1,28 @@
+package gitdemo;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 20;
+		int c=30;
+		int f = 40;
+		int g = 44;
+		System.out.println(c);
+		if(a>b) {
+			System.out.println("A is biggest nmber");
+			
+		}else
+		{
+			System.out.println("B is biggest number");
+			
+		}
+
+	}
+
+
+	}
+
+
