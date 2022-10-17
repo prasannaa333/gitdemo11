7ce686769b3276c5bb133e46e17ed47145979bcf
Prathyusha <DELL@DESKTOP-8ALRF12>
2022-10-17 16:41:08 +0530

    edi last e gaa?????

---
+++ gitdemo/src/gitdemo/Test6.java
@@ -0,0 +1,25 @@
+package gitdemo;
+
+public class Test6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c=30;
+		int f = 40;
+		int g = 44;
+		int vvv= 678;
+		System.out.println(c);
+		if(a>b) {
+			System.out.println("A is biggest nmber");
+			
+		}else
+		{
+			System.out.println("B is biggest number");
+			
+		
+
+	}
+
+}
